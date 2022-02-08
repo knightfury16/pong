ecba7a320da3f9d7b09ed137c37e1df05b7abefb
knightfury1493 <suhaibahmmad1493@gmail.com>
2022-02-08 14:06:47 +0600

    Added AIRandomness class

---
+++ src/AIRandomness.js
@@ -0,0 +1,11 @@
+class AiRandomness{
+
+
+
+	randomTolerence(paddleHeight){
+		return round(random(5,paddleHeight/2));
+	}
+
+
+
+}
--- src/paddleAI.js
+++ src/paddleAI.js
@@ -1,12 +1,12 @@
-class PaddleAI{
+class PaddleAI extends AiRandomness{
 
 	constructor(p1){
+		super();
 		this.p1 = p1;
-
 	}
 
 	AI(puck){
-		
+
 		this.paddleCenter = createVector(p1.offsetGap + (p1.paddleWidth/2), p1.pos + (p1.paddleHeight/2));
 
 		if( puck.vel.x < 0){
@@ -15,30 +15,14 @@ class PaddleAI{
 			let y = puck.pos.y + tan(puck.vel.heading()) * (x - puck.pos.x);
 			this.intersectionPoint = createVector(x,y);
 	
-			// console.log('PC = ' + this.paddleCenter.y + ' PC + 35 = '+ (this.paddleCenter.y+35) + ' IP = ' + this.intersectionPoint.y);
-
-			// console.log(this.intersectionPoint.y,this.paddleCenter.y);
-
-			if(this.paddleCenter.y < this.intersectionPoint.y && this.paddleCenter.y + 35 < this.intersectionPoint.y){
-				// doDown
-				this.p1.down(this.p1);
-				// console.log('Going Down');
-				
+			if(this.paddleCenter.y < this.intersectionPoint.y && 
+				this.paddleCenter.y + this.randomTolerence(this.p1.paddleHeight) < this.intersectionPoint.y){
+					this.p1.down(this.p1);
 			}
-			else if(this.paddleCenter.y > this.intersectionPoint.y  && this.paddleCenter.y - 35 > this.intersectionPoint.y){
-				// goUp
-				this.p1.up(this.p1);
+			else if(this.paddleCenter.y > this.intersectionPoint.y  && 
+				this.paddleCenter.y - this.randomTolerence(this.p1.paddleHeight) > this.intersectionPoint.y){
+					this.p1.up(this.p1);
 			}
-
 		}
 	}
-
-	calculateDistance(puck){
-		return dist(this.intersectionPoint.x,this.intersectionPoint.y,puck.pos.x,puck.pos.y);
-	}
-
-
-
-
-
 }
