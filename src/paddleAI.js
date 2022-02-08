b93eddf30ee214a67a8026f6a3280aeda0c67398
knightfury1493 <suhaibahmmad1493@gmail.com>
2022-02-08 13:51:40 +0600

    Basic Ai movement done.

--- src/paddleAI.js
+++ src/paddleAI.js
@@ -1,32 +1,31 @@
 class PaddleAI{
 
 	constructor(p1){
-		// Distance between the intersection point and puck center
-		this.puckdist = Infinity;
 		this.p1 = p1;
-		
-		this.paddleCenter = createVector(p1.offsetGap + (p1.paddleWidth/2), p1.pos + (p1.paddleHeight/2));
 
 	}
 
 	AI(puck){
 		
-		let x = this.p1.offsetGap;
-		let y = puck.pos.y + tan(puck.vel.heading()) * (x - puck.pos.x);
-		this.intersectionPoint = createVector(x,y);
-		
+		this.paddleCenter = createVector(p1.offsetGap + (p1.paddleWidth/2), p1.pos + (p1.paddleHeight/2));
 
-		if( this.calculateDistance(puck) < this.puckdist){
+		if( puck.vel.x < 0){
 
-			console.log(this.calculateDistance(puck));
-			this.puckdist = this.calculateDistance(puck);
+			let x = this.p1.offsetGap;
+			let y = puck.pos.y + tan(puck.vel.heading()) * (x - puck.pos.x);
+			this.intersectionPoint = createVector(x,y);
+	
+			// console.log('PC = ' + this.paddleCenter.y + ' PC + 35 = '+ (this.paddleCenter.y+35) + ' IP = ' + this.intersectionPoint.y);
 
-			if(this.intersectionPoint.y > this.paddleCenter.y ){
+			// console.log(this.intersectionPoint.y,this.paddleCenter.y);
+
+			if(this.paddleCenter.y < this.intersectionPoint.y && this.paddleCenter.y + 35 < this.intersectionPoint.y){
 				// doDown
 				this.p1.down(this.p1);
+				// console.log('Going Down');
 				
 			}
-			else if(this.intersectionPoint.y < this.paddleCenter.y){
+			else if(this.paddleCenter.y > this.intersectionPoint.y  && this.paddleCenter.y - 35 > this.intersectionPoint.y){
 				// goUp
 				this.p1.up(this.p1);
 			}
